f3a9b7bfa5543eb5b2bf4d62f3a587e0b625fc6b
Dave Bush <davebush@gmail.com>
2022-09-22 08:13:07 -0400

    Fixed some missing transactions
    Signed-off-by: Dave Bush <davebush@gmail.com>

--- import-transactions.js
+++ import-transactions.js
@@ -4,6 +4,8 @@ import util from 'util';
 
 const tags = {
   food: 'Food',
+  personalCare: 'Personal Care Expenses',
+  pizza: 'Pizza',
   grocery: 'Grocery',
   costco: 'Costco',
   wine: 'Wine',
@@ -14,9 +16,11 @@ const tags = {
   fuel: 'Auto Fuel',
   auto: 'Auto Expenses',
   communication: 'Communication Expenses',
+  mobile: 'Mobile Phone Expenses',
   cottage: 'Cottage',
   hardware: 'Hardware Store',
   depot: 'Home Depot',
+  lowes: 'Lowes',
   charity: 'Charitable Donation',
   onlineServices: 'Online Services',
   costco: 'Costco',
@@ -39,16 +43,22 @@ const tags = {
   ikea: 'Ikea',
   house: 'House Expenses',
   dental: 'Dental Expenses',
-  medical: 'Medical Expenses'
+  medical: 'Medical Expenses',
+  pet: 'Pet Expenses',
+  ct: 'Candian Tire'
 }
 
 const profile = [
    // Household
    { name: 'THE HOME DEPOT #', tags: [tags.hardware, tags.depot, tags.house]},
+   { name: 'LOWES #', tags: [tags.hardware, tags.lowes, tags.house]},
    { name: 'PUREWATER HOME OASIS', tags: [tags.pool, tags.house]},
    { name: 'IKEA', tags: [tags.funriture, tags.ikea, tags.house]},
    { name: 'WWW.IKEA.CA', tags: [tags.funriture, tags.ikea, tags.house]},
    { name: 'CANADIAN APPLIANCE SOU', tags: [tags.house]},
+   { name: 'BURNSTOWN BUILDING SUP', tags: [tags.house, tags.cottage]},
+   { name: 'RENFREW HM HWR BLDG CT', tags: [tags.house, tags.cottage]},
+   { name: 'CDN TIRE STORE', tags: [tags.ct, tags.house]},
 
    // Activities
    { name: 'CABELA\'S CANADA', tags: [tags.outdoor]},
@@ -56,6 +66,8 @@ const profile = [
    { name: 'CANADA\'S WONDERLAND', tags: [tags.kidsActivities]},
    { name: 'FUNHAVEN', tags: [tags.kidsActivities]},
    { name: 'STUFFY RIDERS', tags: [tags.kidsActivities]},
+   { name: 'LS CANADIAN MUSEUM OF', tags: [tags.kidsActivities]},
+   { name: 'Canadian Museum of Nat', tags: [tags.kidsActivities]},
 
    // Clothing
    { name: 'BROWNS SHOES', tags: [tags.kidsClothing, tags.clothing]},
@@ -63,9 +75,10 @@ const profile = [
    { name: 'SUZY SHIER', tags: [tags.clothing]},
    { name: 'SHOE COMPANY #', tags: [tags.clothing, tags.shoes]},
    { name: 'H&M CA', tags: [tags.clothing]},
-   { name: 'BENTLEY #', tags: [tags.clothing]},
+   { name: 'BENTLEY ', tags: [tags.clothing]},
    { name: 'URBAN PLANET #', tags: [tags.clothing, tags.KidsClothing]},
    { name: 'LITTLE BURGUNDY', tags: [tags.clothing, tags.KidsClothing, tags.shoes]},
+   { name: 'SHEIN.COM', tags: [tags.clothing]},
 
 
    // Financial Services
@@ -77,6 +90,7 @@ const profile = [
    // Travel Expenses
    { name: 'ALOFT VAUGHAN MILLS', tags: [tags.travel, tags.hotel] },
    { name: 'FAIRMONT CHATEAU LAURI', tags: [tags.travel, tags.hotel] },
+   { name: 'ROYAL YORK HOTEL', tags: [tags.travel, tags.hotel] },
 
    // Dental Expenses
    { name: 'COLISEUM DENTAL', tags: [tags.medical, tags.dental] },
@@ -88,32 +102,42 @@ const profile = [
    { name: 'TRADINGVIEW1PRODUCT', tags: [tags.profit] },
    { name: 'AIRDNA MARKET DATA DENVER CO', tags: [tags.profit] },
 
+   // Personal Care Expenses
+   { name: 'BATH AND BODY WORKS #', tags: [tags.personalCare] },
+
    // Gas Stations
    { name: 'ESSO', tags: [tags.fuel, tags.auto] },
    { name: 'SHELL', tags: [tags.fuel, tags.auto] },
    { name: 'EAGLES NEST GAS BAR', tags: [tags.fuel, tags.auto] },
    { name: 'CDN TIRE GASBAR ', tags: [tags.fuel, tags.auto] },
-   { name: 'RALPH & SONS ', tags: [tags.fuel, tags.auto] },
-   { name: 'PETROCAN ', tags: [tags.fuel, tags.auto] },
+   { name: 'RALPH & SONS', tags: [tags.fuel, tags.auto] },
+   { name: 'RALPH AND SONS', tags: [tags.fuel, tags.auto] },
+   { name: 'PETROCAN', tags: [tags.fuel, tags.auto] },
    { name: 'FINN\'S GAS BAR ', tags: [tags.fuel, tags.auto] },
 
    // Restaurants
    { name: 'REDNECK BISTRO', tags: [tags.food, tags.restaurants] },
    { name: 'HOCKEY SUSHI KANATA', tags: [tags.food, tags.restaurants]},
    { name: 'JADE YI\'S KITCHEN', tags: [tags.food, tags.restaurants]},
-   { name: 'KANATA NOODLE HOUSE ', tags: [tags.food, tags.restaurants]},
+   { name: 'KANATA NOODLE HOUSE', tags: [tags.food, tags.restaurants]},
    { name: 'JACK ASTOR\'S ', tags: [tags.food, tags.restaurants]},
    { name: 'THE KEG STEAKHOUSE ', tags: [tags.food, tags.restaurants]},
-   { name: 'SHAWARMA HOUSE ', tags: [tags.food, tags.restaurants]},
+   { name: 'SHAWARMA HOUSE', tags: [tags.food, tags.restaurants]},
 
 
    // Fast Food Restaurants
-   { name: 'PIZZA PIZZA # ', tags: [tags.food, tags.restaurants, tags.fastFood] },
+   { name: 'PIZZA PIZZA #', tags: [tags.food, tags.restaurants, tags.fastFood, tags.pizza] },
+   { name: 'LITTLE CAESARS PIZZA', tags: [tags.food, tags.restaurants, tags.fastFood, tags.pizza] },
+   { name: 'CALABOGIE PIZZARIA', tags: [tags.food, tags.restaurants, tags.fastFood, tags.pizza] },
    { name: 'MCDONALD\'S #', tags: [tags.food, tags.restaurants, tags.fastFood] },
    { name: 'Subway ', tags: [tags.food, tags.restaurants, tags.fastFood] },
    { name: 'DAIRY QUEEN ', tags: [tags.food, tags.restaurants, tags.fastFood] },
-   { name: 'KOKOMISH CAFE ', tags: [tags.food, tags.restaurants, tags.fastFood] },
+   { name: 'KOKOMISH CAFE', tags: [tags.food, tags.restaurants, tags.fastFood] },
    { name: 'KFC/TACO BELL ', tags: [tags.food, tags.restaurants, tags.fastFood] },
+   { name: 'A&W ', tags: [tags.food, tags.restaurants, tags.fastFood] },
+   { name: 'NEW YORK FRIES #', tags: [tags.food, tags.restaurants, tags.fastFood] },
+
+   // Coffee Shops
    { name: 'STARBUCKS ', tags: [tags.food, tags.restaurants, tags.fastFood, tags.coffeeShops] },
    { name: 'TIM HORTONS ', tags: [tags.food, tags.restaurants, tags.fastFood, tags.coffeeShops]},
    { name: 'SECOND CUP ', tags: [tags.food, tags.restaurants, tags.fastFood, tags.coffeeShops]},
@@ -127,10 +151,14 @@ const profile = [
    { name: 'WAL-MART #', tags: [tags.food, tags.grocery]},
    { name: 'THE BUTCHERY', tags: [tags.food, tags.grocery]},
    { name: 'FOODLAND', tags: [tags.food, tags.grocery]},
+   { name: 'BULK BARN #', tags: [tags.food, tags.grocery]},
+   { name: 'M&M FOOD MARKET #', tags: [tags.food, tags.grocery]},
+   { name: 'BRANDON & MEGAN\'S NO F', tags: [tags.food, tags.grocery]},
 
    // Cody
-   { name: 'REN\'S PETS', tags: [tags.cody]},
-   { name: 'CARLING ANIMAL HOSPITA', tags: [tags.cody]},
+   { name: 'REN\'S PETS', tags: [tags.cody, tags.pet]},
+   { name: 'CARLING ANIMAL HOSPITA', tags: [tags.cody, tags.pet]},
+   { name: 'CRITTER JUNGLE', tags: [tags.pet]},
 
    // LCBO
    { name: 'LCBO/RAO #', tags: [tags.food,tags.wine] },
@@ -147,7 +175,9 @@ const profile = [
    { name: 'Audible CA', tags: [tags.onlineServices] },
 
    // Communications
-   { name: 'KOODO MOBILE PAC', tags: [tags.communication] },
+   { name: 'KOODO MOBILE PAC', tags: [tags.communication, tags.mobile] },
+   { name: 'KOODO AIRTIME', tags: [tags.communication, tags.mobile] },
+   { name: 'KOODO TOP UP REAPPROV', tags: [tags.communication, tags.mobile] },
    { name: 'ROGERS ', tags: [tags.communication] },
 
    // Amazon
@@ -156,13 +186,14 @@ const profile = [
 
    // Apple
    { name: 'APPLE.COM/BILL', tags: [tags.apple] },
+   { name: 'APPLE STORE #', tags: [tags.apple] },
 
    // Online Education
    { name: 'DAPP UNIVERSITY NASHVILLE TN', tags: [tags.education, tags.profit] },
    { name: 'LEARN PLAN PROF NEW YORK NY', tags: [tags.education, tags.profit] },
 
    // Charitable Donations
-   { name: 'SQ *STTR FUNDRAISER', tags: [tags.charity] },
+   { name: 'STTR FUNDRAISER', tags: [tags.charity] },
 
  ];
 
